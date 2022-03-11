7d69a3b28e3bb6fbec2fa6198e7624e3d9e3e8da
Eryalus <adriancamesellemartin@gmail.com>
2022-03-11 22:38:23 +0100

    Add talking cat

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,29 +1,27 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
-import { takeUntil } from 'rxjs/operators';
 
 import { AccountService } from 'app/core/auth/account.service';
-import { Account } from 'app/core/auth/account.model';
 
 @Component({
   selector: 'jhi-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit, OnDestroy {
-  account: Account | null = null;
+export class HomeComponent implements OnDestroy {
+  catSayingPrefix = 'home.cat.';
+  randomTexts = ['meow1', 'meow2', 'meow3', 'prr1', 'prr2', 'hiss1', 'hiss2'];
+  firstsTexts = ['greeting', 'name'];
+  catSaying = '';
+  toggleCount = 0;
+  textBackoff = 1500;
+  showQuestSign = true;
+  showCatFrame = false;
 
   private readonly destroy$ = new Subject<void>();
 
   constructor(private accountService: AccountService, private router: Router) {}
-
-  ngOnInit(): void {
-    this.accountService
-      .getAuthenticationState()
-      .pipe(takeUntil(this.destroy$))
-      .subscribe(account => (this.account = account));
-  }
 
   login(): void {
     this.router.navigate(['/login']);
@@ -32,5 +30,51 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
+  }
+
+  catPress(): void {
+    if (this.showQuestSign) {
+      const toggle = this.updateCatText();
+      this.toggleQuestLogo();
+      this.toggleCatFrame();
+      setTimeout(() => this.showTextBackoff(toggle), this.randomTime(this.textBackoff, 1500));
+    }
+  }
+
+  updateCatText(): number {
+    if (this.toggleCount < this.firstsTexts.length) {
+      this.catSaying = this.catSayingPrefix + this.firstsTexts[this.toggleCount];
+      this.toggleCount++;
+    } else {
+      this.catSaying = this.catSayingPrefix + this.randomTexts[Math.floor(this.randomTexts.length * Math.random())];
+      this.toggleCount++;
+    }
+    return this.toggleCount - 1;
+  }
+
+  randomTime(base: number, random: number): number {
+    return base + random * Math.random();
+  }
+
+  showTextBackoff(toggle: number): void {
+    if (toggle < this.firstsTexts.length) {
+      this.toggleAll();
+    } else {
+      this.toggleCatFrame();
+      setTimeout(() => this.toggleQuestLogo(), this.randomTime(1000, 4000));
+    }
+  }
+
+  toggleAll(): void {
+    this.toggleQuestLogo();
+    this.toggleCatFrame();
+  }
+
+  toggleQuestLogo(): void {
+    this.showQuestSign = !this.showQuestSign;
+  }
+
+  toggleCatFrame(): void {
+    this.showCatFrame = !this.showCatFrame;
   }
 }
