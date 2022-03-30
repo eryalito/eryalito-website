8f06980c48f6519a046010f72d03b3951e258e10
Eryalus <adriancamesellemartin@gmail.com>
2022-03-31 00:23:39 +0200

    Fix mail tests
    Signed-off-by: Eryalus <adriancamesellemartin@gmail.com>

--- src/test/java/dev/eryalus/web/service/MailServiceIT.java
+++ src/test/java/dev/eryalus/web/service/MailServiceIT.java
@@ -145,7 +145,7 @@ class MailServiceIT {
         assertThat(message.getSubject()).isEqualTo("test title");
         assertThat(message.getAllRecipients()[0]).hasToString(user.getEmail());
         assertThat(message.getFrom()[0]).hasToString(jHipsterProperties.getMail().getFrom());
-        assertThat(message.getContent().toString()).isEqualToNormalizingNewlines("<html>test title, http://127.0.0.1:8080, john</html>\n");
+        assertThat(message.getContent().toString()).isEqualToNormalizingNewlines("<html>test title, http://eryalus.dev:8080, john</html>\n");
         assertThat(message.getDataHandler().getContentType()).isEqualTo("text/html;charset=UTF-8");
     }
 
@@ -224,7 +224,7 @@ class MailServiceIT {
             String emailTitle = (String) properties.get("email.test.title");
             assertThat(message.getSubject()).isEqualTo(emailTitle);
             assertThat(message.getContent().toString())
-                .isEqualToNormalizingNewlines("<html>" + emailTitle + ", http://127.0.0.1:8080, john</html>\n");
+                .isEqualToNormalizingNewlines("<html>" + emailTitle + ", http://eryalus.dev:8080, john</html>\n");
         }
     }
 
